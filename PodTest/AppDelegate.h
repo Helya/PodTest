95e09e0b280439b74e7fa356fe9a835c5364cc0f
Helya <poweryumx@163.com>
2016-04-05 11:14:31 +0800

    Initial Commit

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by 余敏侠 on 16/4/5.
+//  Copyright © 2016年 secrui Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PodTest
+//
+//  Created by 余敏侠 on 16/4/5.
+//  Copyright © 2016年 secrui Co.,Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
